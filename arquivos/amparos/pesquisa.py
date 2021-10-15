4b0dfe9f855cce851f4f1e8eb6253f0a9a66ffe2
Rúben Israel <rubenisrael974@gmail.com>
2021-10-14 22:30:51 -0300

    ACHO que terminei a logica :D

--- arquivos/amparos/pesquisa.py
+++ arquivos/amparos/pesquisa.py
@@ -8,11 +8,13 @@ from selenium import webdriver
 from time import sleep
 
 opc = Options()
-opc.headless = False # Esconder navegador? True or False
+opc.headless = True # Esconder navegador? True or False
 
-encontrou, tempo, cont_pesq, resultado = True, 0, 1, {}
+encontrou, tempo, fim_cont_pesq, ini_cont_pesq, resultado = True, 0, 1, 0, {}
 
-resultado = {
+resultado = {'sua pergunta': input('Digite a sua pergunta: ')}
+
+'''resultado = {
     'sua pergunta': 'resumo segunda guerra mundial',
     '1 pergunta similar': '',
     '1 pergunta similar 1 resposta': '',
@@ -20,88 +22,98 @@ resultado = {
     '2 pergunta similar': '',
     '2 pergunta similar 1 resposta': '',
     '2 pergunta similar 2 resposta': '',
-}
+}'''
 
-primeira_res = (True, False)
-segunda_res = (False, False)
+primeira_res = (True, True)
+segunda_res = (True, True)
 
 if True in segunda_res:
     
-    cont_pesq = 2
+    fim_cont_pesq = 2
 
-for c in range(0, cont_pesq):
+if True not in primeira_res:
 
-    try:
+    ini_cont_pesq = 1
 
-        driver = webdriver.Firefox(executable_path='arquivos/driver/geckodriver.exe', options=opc)
+if True not in primeira_res and True not in segunda_res:
 
-    except selenium.common.exceptions.WebDriverException:
+    pass
 
-        print('O arquivo geckodrive.exe não foi encontrado, verifique a pasta "arquivos/driver/"')
+else:
 
-    try:
+    for c in range(ini_cont_pesq, fim_cont_pesq):
 
-        driver.get(f'https://brainly.com.br/app/ask?q={resultado["sua pergunta"]}')
+        try:
 
-    except selenium.common.exceptions.WebDriverException:
+            driver = webdriver.Firefox(executable_path='arquivos/driver/geckodriver.exe', options=opc)
 
-        print('Não foi possivel acessar o site, verifique a sua internet')
+        except selenium.common.exceptions.WebDriverException:
 
-    else:
+            print('O arquivo geckodrive.exe não foi encontrado, verifique a pasta "arquivos/driver/"')
 
-        while True:
+        try:
 
-            try:
-                
-                sleep(1)
+            driver.get(f'https://brainly.com.br/app/ask?q={resultado["sua pergunta"]}')
 
-                driver.find_elements_by_xpath("//a[@class='sg-text sg-text--small sg-text--link sg-text--bold']")[c].click()
+        except selenium.common.exceptions.WebDriverException:
 
-                break
+            print('Não foi possivel acessar o site, verifique a sua internet')
 
-            except selenium.common.exceptions.InvalidSelectorException:
+        else:
 
-                tempo += 1
+            while True:
 
-                if tempo == 10:
+                try:
+                    
+                    sleep(1)
+
+                    driver.find_elements_by_xpath("//a[@class='sg-text sg-text--small sg-text--link sg-text--bold']")[c].click()
 
                     break
 
-            except IndexError:
+                except selenium.common.exceptions.InvalidSelectorException:
 
-                print(f'A {c}° resposta da sua pergunta não foi encontrada no brainly')
+                    tempo += 1
 
-                encontrou = False
+                    if tempo == 10:
 
-                break
-
-        if tempo <= 10 and encontrou:
-
-            resultado[f'{c+1} pergunta similar']  = driver.find_element_by_xpath("//h1/span[1]").text
-
-            if c == 0:
-
-                inc = primeira_res
-
-            else:
-
-                inc = segunda_res
-
-            if inc[0]:
-
-                resultado[f'{c+1} pergunta similar 1 resposta'] = driver.find_elements_by_xpath("//div[@class='sg-text sg-text--break-words brn-rich-content js-answer-content']")[0].text
-
-            if inc[1]:
-
-                try:
-
-                    resultado[f'{c+1} pergunta similar 2 resposta'] = driver.find_elements_by_xpath("//div[@class='sg-text sg-text--break-words brn-rich-content js-answer-content']")[1].text
+                        break
 
                 except IndexError:
 
-                    resultado[f'{c+1} pergunta similar 2 resposta'] = 'Não há segunda resposta'
+                    print(f'A {c}° resposta da sua pergunta não foi encontrada no brainly')
 
-            driver.close()
+                    encontrou = False
 
-for k, i in resultado.items():
+                    break
+
+            if tempo <= 10 and encontrou:
+
+                resultado[f'{c+1} pergunta similar']  = driver.find_element_by_xpath("//h1/span[1]").text
+
+                if c == 0:
+
+                    inc = primeira_res
+
+                else:
+
+                    inc = segunda_res
+
+                if inc[0]:
+
+                    resultado[f'{c+1} pergunta similar 1 resposta'] = driver.find_elements_by_xpath("//div[@class='sg-text sg-text--break-words brn-rich-content js-answer-content']")[0].text
+
+                if inc[1]:
+
+                    try:
+
+                        resultado[f'{c+1} pergunta similar 2 resposta'] = driver.find_elements_by_xpath("//div[@class='sg-text sg-text--break-words brn-rich-content js-answer-content']")[1].text
+
+                    except IndexError:
+
+                        resultado[f'{c+1} pergunta similar 2 resposta'] = 'Não há segunda resposta'
+
+                driver.close()
+
+    for k, i in resultado.items():
         print(f'\nChave: {k}\nItem: {i}\n')
