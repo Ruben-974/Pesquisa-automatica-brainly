ca326d65e0e9bfee1121e9002c81b0e9e7c8fc05
Rúben Israel <87450219+Ruben-974@users.noreply.github.com>
2021-11-10 19:56:22 -0300

    Baniram meu IP :D amanhã continuo

--- arquivos/amparos/pesquisa_sem_driver.py
+++ arquivos/amparos/pesquisa_sem_driver.py
@@ -4,55 +4,91 @@ from bs4 import BeautifulSoup
 from googlesearch import search
 from time import sleep
 
+pergunta = 'resumo segunda guerra mundial'
+primeira_res = (True, False)
+segunda_res = (False, False)
+
 links = []
 
-respostas = {'sua pergunta': 'resumo segunda guerra mundial',
+inc, fim = 0, 2
+
+respostas = {'sua pergunta': pergunta,
              '1 pergunta similar': '',
              '1 pergunta similar 1 resposta': '',
              '1 pergunta similar 2 resposta': '',
              '2 pergunta similar': '',
              '2 pergunta similar 1 resposta': '',
              '2 pergunta similar 2 resposta': ''}
- 
+
 quest = respostas['sua pergunta'] + ' site:brainly.com.br'
 
-for l in search(quest, tld='co.in', num=2, stop=2, pause=10):
+if True not in primeira_res and True not in segunda_res:
 
-    links.append(l)
+    quant = 0
 
-for c in range(len(links)):
+elif True in primeira_res and True in segunda_res:
 
-    sleep(5)
+    quant = 2
 
-    html = Request(links[c], headers={'User-Agent': 'Mozilla/5.0'})
+else:
 
-    html = urlopen(html).read()
+    quant = 1
 
-    html = BeautifulSoup(html, 'html.parser')
+if quant == 1:
 
-    resp = html.findAll('div', {'class': 'brn-qpage-next-answer-box__content'})
+    if True in primeira_res:
 
-    perg = html.find('span', {'class': 'sg-text sg-text--large sg-text--bold sg-text--break-words brn-qpage-next-question-box-content__primary'})
+        inc = 0
 
-    respostas[f'{c+1} pergunta similar'] = perg.get_text()
+    else:
 
-    for c2 in range(len(resp)):
-        
-        for c3 in resp[c2].find_all('p'):
+        inc = 1
 
-            respostas[f'{c+1} pergunta similar {c2+1} resposta'] += c3.get_text() + '\n'
+print(quant, inc)
 
-        respostas[f'{c+1} pergunta similar {c2+1} resposta'] = respostas[f'{c+1} pergunta similar {c2+1} resposta'].strip()
+if quant != 0:
 
-    if respostas[f'{c+1} pergunta similar 1 resposta'] == '':
-        
-        respostas[f'{c+1} pergunta similar 1 resposta'] = resp[0].get_text().strip()
+    for l in search(quest, tld='co.in', start=inc, num=quant, pause=30, user_agent='Mozilla/5.0'):
 
-    if respostas[f'{c+1} pergunta similar 2 resposta'] == '':
+        links.append(l)
 
-        respostas[f'{c+1} pergunta similar 2 resposta'] = resp[1].get_text().strip()
+    print(links)
 
-for k, v in respostas.items():
+    exit()
 
-    print(f'Chave: {k}\n\nItem: {v}\n')
+    for c in range(len(links)):
+
+        sleep(5)
+
+        html = Request(links[c], headers={'User-Agent': 'Mozilla/5.0'})
+
+        html = urlopen(html).read()
+
+        html = BeautifulSoup(html, 'html.parser')
+
+        resp = html.findAll('div', {'class': 'brn-qpage-next-answer-box__content'})
+
+        perg = html.find('span', {'class': 'sg-text sg-text--large sg-text--bold sg-text--break-words brn-qpage-next-question-box-content__primary'})
+
+        respostas[f'{c+1} pergunta similar'] = perg.get_text()
+
+        for c2 in range(len(resp)):
+            
+            for c3 in resp[c2].find_all('p'):
+
+                respostas[f'{c+1} pergunta similar {c2+1} resposta'] += c3.get_text() + '\n'
+
+            respostas[f'{c+1} pergunta similar {c2+1} resposta'] = respostas[f'{c+1} pergunta similar {c2+1} resposta'].strip()
+
+        if respostas[f'{c+1} pergunta similar 1 resposta'] == '':
+            
+            respostas[f'{c+1} pergunta similar 1 resposta'] = resp[0].get_text().strip()
+
+        if respostas[f'{c+1} pergunta similar 2 resposta'] == '':
+
+            respostas[f'{c+1} pergunta similar 2 resposta'] = resp[1].get_text().strip()
+
+    for k, v in respostas.items():
+
+        print(f'Chave: {k}\n\nItem: {v}\n')
 
